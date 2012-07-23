b2fdaafe127b106b2741cc0c47eff5ce0bb04220
Markus Kirschner <Markus.Kirschner@dressyco.de>
2012-07-23 19:23:05 +0200

    Added retina images

---
+++ OSX/CondensePNGs/DrcWindowController.h
@@ -0,0 +1,13 @@
+//
+//  DrcWindowController.h
+//  CondensePNGs
+//
+//  Created by Markus Kirschner on 23.07.12.
+//  Copyright (c) 2012 dressyco.de. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DrcWindowController : NSWindowController
+
+@end
